07507edda3280da895b1ef04bce276b136dd09fd
Aidan Nagorcka-Smith <aidanns@gmail.com>
2018-07-12 22:35:09 +1000

    Fix bug in download test script.

--- .travis/download-test-script.sh
+++ .travis/download-test-script.sh
@@ -3,7 +3,8 @@
 pushd $(dirname $0)/.. > /dev/null
 
 TEST_SCRIPT_URL="https://gist.githubusercontent.com/aidanns/a37ab5d09840253ef1d74218b3e739fb/raw/33d24b122c795c77c9a90ae61fc2a8cf046ba71e/ansible-role-test.sh"
-wget -O ./test/test.sh ${TEST_SCRIPT_URL}
+
+wget -O ./tests/test.sh ${TEST_SCRIPT_URL}
 chmod +x ./tests/test.sh
 
 popd > /dev/null
