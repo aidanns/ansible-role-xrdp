8bc70beeeb4b2f3ff9f64e5709ca3ea96bebb29b
Aidan Nagorcka-Smith <aidanns@gmail.com>
2018-07-12 22:27:31 +1000

    Update to new version of test script.

--- .travis/download-test-script.sh
+++ .travis/download-test-script.sh
@@ -2,7 +2,7 @@
 
 pushd $(dirname $0)/.. > /dev/null
 
-wget -O ./tests/test.sh https://gist.githubusercontent.com/aidanns/a37ab5d09840253ef1d74218b3e739fb/raw/2dc8100bab92da745c7a7b38990df2a3546048d0/ansible-role-test.sh
+wget -O ./tests/test.sh https://gist.githubusercontent.com/aidanns/a37ab5d09840253ef1d74218b3e739fb/raw/e9997bee768e0c4847c987ea27c0ca7f717e1f86/ansible-role-test.sh
 chmod +x ./tests/test.sh
 
 popd > /dev/null
